de85ab3a3128b13b8c0531a2661380ed68f1e8b0
Prince Mishra <princemishraa588@gmail.com>
2023-08-13 20:19:13 +0530

    Create Assignment_1.py

---
+++ Assignment_1.py
@@ -0,0 +1,57 @@
+# 1. Write a code to print a statement "Hello,Darkness"
+print("Hello, Darkness")
+
+# 2. Declare 10 variables and perform arithmetic operations(Addition,Multiplication,Division) on them.
+# Declare 10 variables
+a = 5
+b = 10
+c = 3
+d = 7
+e = 12
+f = 2
+g = 8
+h = 15
+i = 6
+j = 4
+
+# Perform arithmetic operations
+add_result = a + b
+multiply_result = c * d
+divide_result = e / f
+add_divide_result = g + h / i
+multiply_add_result = j * (a + b)
+
+# Print the results
+print("Addition Result:", add_result)
+print("Multiplication Result:", multiply_result)
+print("Division Result:", divide_result)
+print("Addition and Division Result:", add_divide_result)
+print("Multiplication and Addition Result:", multiply_add_result)
+
+
+# 3. Declare 10 variables and perform arithmetic operations(Exponential, Modulus and Floor Division) on them.
+# Declare 10 variables
+a = 50
+b = 10
+c = 3
+d = 7
+e = 12
+f = 2
+g = 8
+h = 15
+i = 6
+j = 4
+
+# Perform arithmetic operations
+exponential_result = a ** b
+modulus_result = c % d
+floor_division_result = e // f
+exponential_modulus_result = g ** (h % i)
+modulus_floor_result = j % (a // b)
+
+# Print the results
+print("Exponential Result:", exponential_result)
+print("Modulus Result:", modulus_result)
+print("Floor Division Result:", floor_division_result)
+print("Exponential and Modulus Result:", exponential_modulus_result)
+print("Modulus and Floor Division Result:", modulus_floor_result)
