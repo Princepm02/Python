5fb347c0f56895dfd34244e9e16f80e586b55ee7
Prince Mishra <princemishraa588@gmail.com>
2023-08-16 20:06:47 +0530

    Create Assignment_2.py

---
+++ Assignment_2.py
@@ -0,0 +1,12 @@
+# Q.1. Print the length of the string 'programmer'
+x="programmer"
+print(len(x))
+
+# Q.2. Print the following string by using escape characters : We are "VIKINGS" of the north.
+print("We are \"VIKINGS\" of the north.")
+
+# Q.3. Write a code to loop through a list of numbers and print it
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers:
+    print(number)
